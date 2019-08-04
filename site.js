2e1731959d51cd88781b7450f002a8f4dd8265b3
ray-med <53706531+ray-med@users.noreply.github.com>
2019-08-04 21:05:38 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,9 @@
+avatar_button = document.getElementById(avatar_button)
+
+function avatarization(){}
+
+function test(){
+	var x = document.getElementById('txt');
+	var n = x.innerHTML;
+	n = n/2;
+}
